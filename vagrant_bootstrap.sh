8d32cfa4b6c4b8eee961da206b8b64b52b0481ac
jcuga <justinpcuga@gmail.com>
2017-12-07 22:50:19 -0500

    first commit

---
+++ vagrant_bootstrap.sh
@@ -0,0 +1,24 @@
+# instructions from: https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-docker-ce
+
+echo "Installing Docker..."
+sudo apt-get update
+
+sudo apt-get install -y \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
